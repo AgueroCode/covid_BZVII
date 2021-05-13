3db1fa81510c4e8f317342e2105aa139659baf8a
AgueroCode <gomezdeaguero@gmail.com>
2021-05-13 22:00:04 +0200

    readme.md

--- covid/__init__.py
+++ covid/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #la app hay que crearla aqui
 
-@app.route("/")
-def index():
-    return "Flask esta funcionando!"
+from covid import views #aqui nos traemos las rutas que vamos a ir guardando en views
