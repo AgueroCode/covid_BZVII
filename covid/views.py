944954d36b739bfbdfb33178a640fac6586b7058
AgueroCode <gomezdeaguero@gmail.com>
2021-05-20 16:10:34 +0200

    Final flask Covid

--- covid/views.py
+++ covid/views.py
@@ -1,6 +1,9 @@
+from os import error
+from flask import render_template, request #va a la carpeta template, busca el template y lo devuelve - request es una instancia en la que esta toda la informacion de la peticion
 from covid import app
 import csv
 import json
+from datetime import date
 
 @app.route("/provincias")
 def provincias():
@@ -62,4 +65,49 @@ def casos(year, mes, dia):
 
     fichero.close()
     return json.dumps(res)
+
+@app.route("/incidenciasdiarias", methods = ['GET', 'POST'])
+def incidencia():
+    formulario = {
+        'provincia': '',
+        'fecha': str(date.today()),
+        'num_casos_prueba_pcr': 0,
+        'num_casos_prueba_test_ac': 0,
+        'num_casos_prueba_ag': 0,
+        'num_casos_prueba_elisa': 0,
+        'num_casos_prueba_desconocida': 0
+    }
+
+    fichero = open('data/provincias.csv', 'r', encoding="utf8")
+    csvreader = csv.reader(fichero, delimiter=",")
+    # next(csvreader)
+    lista = []
+    for registro in csvreader: 
+        d = {'codigo': registro[0], 'descripcion': registro[1]}
+        lista.append(d)
+
+    fichero.close()
+
+    if request.method == 'GET':
+        return render_template("alta.html", datos=formulario, 
+                               provincias=lista, error="") #aqui renderiza el template que hemos creado en templates - datos es lo que va a usar (es la clave) jinja basado en el formulario (que es el valor)
+    
+    for clave in formulario:  #aqui vamos a informar el formulario para que 'recuerde' los datos
+        formulario[clave] = request.form[clave]      
+
+    #validamos que num_casos en general es entero no negativo
+    num_pcr = request.form['num_casos_prueba_pcr']
+    try:
+        num_pcr = int(num_pcr)
+        if num_pcr < 0:
+            raise ValueError('Debe ser positivo') #raise lanza una excepcion de forma automatica
+    except ValueError: #captura solo el error en ese valor, no cualquier error
+        return render_template("alta.html", datos=formulario, error = "PCR no puede ser negativa") #devolvemos algo al servidor gracias jinja   
+    
+    return "Se ha hecho un post"
+
+
+   
+
+    
 
